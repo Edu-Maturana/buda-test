65ce45a50508ae4494ae8638f0e8922f93849d03
Edu-Maturana <edu.maturana.c@gmail.com>
2024-02-05 18:47:42 -0300

    feat: define controller interface

---
+++ src/spread/adapters/controllers/SpreadControllerInterface.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+interface SpreadControllerInterface {
+  getSpread(req: Request, res: Response): Promise<void>;
+  getAllSpreads(req: Request, res: Response): Promise<void>;
+  setAlertSpread(req: Request, res: Response): Promise<void>;
+  pollAlertSpread(req: Request, res: Response): Promise<void>;
+}
+
+export default SpreadControllerInterface;
